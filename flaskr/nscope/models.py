56c4f10e166002d255aad8a7e565eef09b05e6a6
Theo Lincke <thli2739@colorado.edu>
2020-04-06 15:39:48 -0600

    removed headers from all py files

--- flaskr/nscope/models.py
+++ flaskr/nscope/models.py
@@ -1,9 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Distributed under terms of the MIT license.
-
 """
 Models Example
 """
--- flaskr/nscope/views.py
+++ flaskr/nscope/views.py
@@ -1,11 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2020 theo <theo@behemoth>
-#
-# Distributed under terms of the MIT license.
-
 """
 Views for nscope model
 """
