f2560780cacfd1f2590cad1baa21d7ec096aca59
Liam Mulhall <liammulh@gmail.com>
2022-10-18 13:29:15 -0600

    refactor: simplify `get_valid_oeis_id`
    Also add TODO comment about logging.
    
    For
    https://github.com/numberscope/backscope/pull/56#discussion_r998281771.

--- flaskr/nscope/views.py
+++ flaskr/nscope/views.py
@@ -194,24 +194,21 @@ def factor_oeis_sequence(seq, num_elements):
 
 
 def get_valid_oeis_id(oeis_id):
-    ret = ''
+    valid_id = oeis_id
     if isinstance(oeis_id, str):
         if len(oeis_id) != 7:
             raise Exception('oeis_id not 7 characters in length')
         else:
             first_character = oeis_id[0]
             if first_character.islower():
+                # TODO: This should be logged. See
+                # https://github.com/numberscope/backscope/issues/57.
                 print('info: first character in oeis_id is lowercase')
                 print('info: making first character in oeis_id uppercase')
-                uppercase_oeis_id = first_character.upper()
-                uppercase_oeis_id += oeis_id.partition(first_character)[2]
-                ret = uppercase_oeis_id
-            else:
-                print('info: oeis_id is valid; returning it')
-                ret = oeis_id
+                valid_id[0] = first_character.upper()
     else:
         raise TypeError('oeis_id not a string')
-    return ret
+    return valid_id
 
 @bp.route("/api/get_oeis_values/<oeis_id>/<num_elements>", methods=["GET"])
 def get_oeis_values(oeis_id, num_elements):
