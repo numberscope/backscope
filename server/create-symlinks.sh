a9730b5ea8af6dcc713dacfdfb516cab55a4f0e0
Liam Mulhall <liammulh@gmail.com>
2022-10-24 15:59:54 -0600

    fix: correct typo in symlink script

--- server/create-symlinks.sh
+++ server/create-symlinks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ln -s /home/scope/repos/backscope/server/numberscope.conf /etc/nginx/sites-available/numberscope.conf
 ln -s /etc/nginx/sites-available/numberscope.conf /etc/nginx/sites-enabled/numberscope.conf
-ln -s /home/scope/repos/backscope/server/numberscope.sercice /etc/systemd/system/numberscope.service
+ln -s /home/scope/repos/backscope/server/numberscope.service /etc/systemd/system/numberscope.service
