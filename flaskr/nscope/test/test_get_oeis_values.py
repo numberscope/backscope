ea5dfd2c1ff8eaba836635134c2d9e97797c5847
Aaron Fenyes <fenyes@ihes.fr>
2023-10-10 18:13:16 -0400

    Speed up background work
    By choosing a sequence with fewer references and smaller values

--- flaskr/nscope/test/test_get_oeis_values.py
+++ flaskr/nscope/test/test_get_oeis_values.py
@@ -12,8 +12,8 @@ class TestGetOEISValues(unittest.TestCase):
       views.executor.shutdown()
       print("  Background work done")
     
-    # there will be a long delay after the test runs because
-    # `save_oeis_sequence` is running in the background
+    # we choose A321580 because it currently has small values and few
+    # references, which speeds up the `save_oeis_sequence` background work
     def test_get_oeis_values(self):
       # using test client is recommended in Flask testing how-to
       #   https://flask.palletsprojects.com/en/2.3.x/testing/
@@ -22,22 +22,24 @@ class TestGetOEISValues(unittest.TestCase):
       #   https://github.com/pallets/flask/issues/2949
       with self.app.test_client() as client:
         print("\n  Testing response")
-        endpoint = "http://127.0.0.1:5000/api/get_oeis_values/A000396/10"
+        endpoint = "http://127.0.0.1:5000/api/get_oeis_values/A321580/12"
         response = client.get(endpoint)
         expected_json = {
-          'id': 'A000396',
-          'name': 'A000396 [name not yet loaded]',
+          'id': 'A321580',
+          'name': 'A321580 [name not yet loaded]',
           'values': {
-            '1': '6',
-            '2': '28',
-            '3': '496',
-            '4': '8128',
-            '5': '33550336',
-            '6': '8589869056',
-            '7': '137438691328',
-            '8': '2305843008139952128',
-            '9': '2658455991569831744654692615953842176',
-            '10': '191561942608236107294793378084303638130997321548169216'
+            '1': '1',
+            '2': '2',
+            '3': '4',
+            '4': '8',
+            '5': '10',
+            '6': '12',
+            '7': '16',
+            '8': '18',
+            '9': '24',
+            '10': '26',
+            '11': '28',
+            '12': '32'
           }
         }
         self.assertEqual(response.status_code, 200)
