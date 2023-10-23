cc4d0713ec1c70097b549871225e93488b728de4
Glen Whitney <glen@studioinfinity.org>
2023-10-22 21:27:56 -0700

    chore: Update interdependent dependencies and revise command-line interface (#94)
    * build(deps): bump flask from 1.1.4 to 2.2.5
    
    Bumps [flask](https://github.com/pallets/flask) from 1.1.4 to 2.2.5.
    - [Release notes](https://github.com/pallets/flask/releases)
    - [Changelog](https://github.com/pallets/flask/blob/main/CHANGES.rst)
    - [Commits](https://github.com/pallets/flask/compare/1.1.4...2.2.5)
    
    ---
    updated-dependencies:
    - dependency-name: flask
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: Update interdependent dependencies and revise command-line interface
    
      Updates click, Flask and several of its extensions, itsdangerous, Jinja2,
      MarkupSafe, SQLAlchemy, and Werkzeug.
    
      Removes the obsolete Flask-Script extension, as that has been superseded
      by the built-in Flask command-line interface `flask <COMMAND> ...`
      Reimplements `python manage.py test` as `flask test`.
      Makes progress on #43.
      Supersedes #63.
      Supersedes #90.
    
    * fix: typo
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,30 @@
+import click
+import unittest
 from flaskr import create_app
+
 app = create_app()
+
+# this is a wrapper for 'unittest discover', and the help messages are copied
+# from there
+#   https://docs.python.org/3/library/unittest.html#test-discovery
+@app.cli.command()
+@click.option('-v', '--verbose', 'verbosity',
+  count=True, default=0,
+  help='Verbose output'
+)
+@click.option('-s', '--start-directory', 'start_dir', default='.',
+  help='Directory to start discovery (. default)'
+)
+@click.option('-p', '--pattern', 'pattern', default='test*.py',
+  help='Pattern to match test files (test*.py default)'
+)
+@click.option('-t', '--top-level-directory', 'top_level_dir',
+  help='Top level directory of project (defaults to start directory)'
+)
+def test(verbosity, start_dir, pattern, top_level_dir):
+  test_suite = unittest.defaultTestLoader.discover(
+    start_dir=start_dir,
+    pattern=pattern,
+    top_level_dir=top_level_dir
+  )
+  unittest.TextTestRunner(verbosity=verbosity+1).run(test_suite)
