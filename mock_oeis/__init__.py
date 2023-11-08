20e785c9846f6685d3a54d6e1bee4ab92f202585
Aaron Fenyes <fenyes@ihes.fr>
2023-11-08 17:55:07 -0500

    Mock up B-file endpoint

--- mock_oeis/__init__.py
+++ mock_oeis/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, Blueprint
+import os.path
+from flask import Flask, Blueprint, Response
 
 # application factory guide
 #   https://flask.palletsprojects.com/en/2.3.x/patterns/appfactories/
 
-api = Blueprint('api', __name__, url_prefix='/api')
+api = Blueprint('api', __name__)
 
 @api.route('/ready')
 def ready():
@@ -15,6 +16,19 @@ def test():
     'salutation': 'hello',
     'addressee': 'world'
   }
+
+@api.route("/<oeis_id>/<filename>", methods=["GET"])
+def get_data(oeis_id, filename):
+  expected_filename = f'b{oeis_id[1:]}.txt'
+  if filename == expected_filename:
+    filepath = os.path.join('mock_oeis', 'data', filename)
+    if os.path.isfile(filepath):
+      with open(filepath) as file:
+        return Response(file.read(), mimetype='text/plain')
+    else:
+      return f"Mock OEIS doesn't have sequence {oeis_id}", 404
+  else:
+    return f"B-file for {oeis_id} is called {expected_filename}, not {filename}", 404
 
 def create_app():
   app = Flask(__name__)
