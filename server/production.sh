4ebfc9cb0b80a2c5ecfe8174a8ca02363eb0967e
Glen Whitney <glen@studioinfinity.org>
2023-11-16 13:14:56 -0800

    chore: Update production script to latest setup (#103)
    * chore: Update production script to latest setup
    
    * doc: Carefully record all differences between dev and prod .env

--- server/production.sh
+++ server/production.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/scope/repos/backscope &&
 source .venv/bin/activate &&
-pip install -r requirements.txt &&
-pip install --force cypari2 &&
+sh tools/install-requirements.sh &&
 gunicorn --workers 3 --bind unix:/home/scope/repos/backscope/backscope.sock -m 777 wsgi:app
