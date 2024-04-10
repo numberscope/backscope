b0c55261aa669cd71c39459b3e099e72839b1051
Glen Whitney <glen@studioinfinity.org>
2024-04-09 18:25:55 -0700

    fix: Hot fix for #113 in production

--- server/production.sh
+++ server/production.sh
@@ -2,5 +2,5 @@
 cd /home/scope/repos/backscope &&
 source .venv/bin/activate &&
 sh tools/install-requirements.sh &&
-export GIT_REVISION_HASH=$(git rev-parse --short HEAD) &&
+export GIT_REVISION_HASH=$(sudo -u scope git rev-parse --short HEAD) &&
 gunicorn --workers 3 --bind unix:/home/scope/repos/backscope/backscope.sock -m 777 wsgi:app
