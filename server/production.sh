a328c0f66f14f0fabdd3dd52e0fb696b0a9e5a13
Glen Whitney <glen@studioinfinity.org>
2024-04-13 22:25:54 -0700

    fix: Production hot fix to install gunicorn

--- server/production.sh
+++ server/production.sh
@@ -2,5 +2,6 @@
 cd /home/scope/repos/backscope &&
 source .venv/bin/activate &&
 sh tools/install-requirements.sh &&
+pip install gunicorn &&
 export GIT_REVISION_HASH=$(sudo -u scope git rev-parse --short HEAD) &&
 gunicorn --workers 3 --bind unix:/home/scope/repos/backscope/backscope.sock -m 777 wsgi:app
